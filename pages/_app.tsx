bb2d5261cc25a3d068e4f1ad2d453360c7e8eece
Michael Nolivos <michaelnolivos@gmail.com>
2021-08-31 10:35:57 -0400

    change site title and meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,27 +14,22 @@ function Robes({ Component, pageProps }) {
         `}
       </style>
       <Head>
-        <title>robes.market</title>
+        <title>hoods.market</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@worm_emoji" />
-        <meta property="og:url" content="https://robes.market" />
-        <meta property="og:title" content="robes.market" />
+        <meta name="twitter:creator" content="@micksabox" />
+        <meta property="og:url" content="https://hoods.market" />
+        <meta property="og:title" content="hoods.market" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <meta
           property="og:description"
-          content="See the floor price of Divine Robes from the Loot project."
+          content="See the floor price of Divine Hoods from the Loot project."
         />
-        <meta property="og:image" content="https://robes.market/og.png" />
-        <script
-          data-goatcounter="https://divinerobes.goatcounter.com/count"
-          async
-          src="//gc.zgo.at/count.js"
-        ></script>
+        <meta property="og:image" content="https://hoods.market/og.png" />
       </Head>
     </>
   )
